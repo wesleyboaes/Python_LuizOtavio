1d798715c5f298f69b779334cc7e821f91e1827d
Wesley Boaes <wesleyboaes@gmail.com>
2025-09-08 17:41:19 -0300

    Conditions and Debugger

---
+++ Projeto/aula17.py
@@ -0,0 +1,25 @@
+condition1 = False
+condition2 = False
+condition3 = True
+condition4 = False
+
+if condition1:
+    print('Code for condition 1!')
+    print('Code for condition 1!')
+
+elif condition2:
+    print('Code for condition 2!')
+
+elif condition3:
+    print('Code for condition 3!')
+
+elif condition4:
+    print('Code for condition 4!')
+
+else:
+    print('None of the conditions were satisfied!')
+
+if 10 == 10:
+    print('Another if!')
+
+print('Out of the if!')
