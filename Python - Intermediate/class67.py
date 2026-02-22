ccd6fbf3cd0d9726b25b7ee65e8d3f97eadac214
Wesley Boaes <wesleyboaes@gmail.com>
2026-02-22 16:59:41 -0300

    Default values for parameters

---
+++ Python - Intermediate/class67.py
@@ -0,0 +1,14 @@
+"""
+Default values for parameters
+When defining a function, parameters can have default values.
+If no values is provided for the parameter, the default value will be used.
+Refactor: Edit your code.
+"""
+
+def soma(x, y, z = None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
+
+soma (1, 2)
